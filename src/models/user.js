f6ca8c64962a115bb851409abf3e80e17e553cc7
Carlos Moya <mrcamoga@gmail.com>
2025-09-21 12:43:13 +0200

    Removed duplicated index on email

--- src/models/user.js
+++ src/models/user.js
@@ -2,50 +2,49 @@ import { DataTypes } from "sequelize";
 import { sequelize } from "../config/db.js";
 
 export const User = sequelize.define(
-  "User",
-  {
-    id: {
-      type: DataTypes.BIGINT.UNSIGNED,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    email: {
-      type: DataTypes.STRING(190),
-      allowNull: false,
-      unique: true,
-      validate: { isEmail: true },
-    },
-    role: {
-      type: DataTypes.ENUM("user", "admin"),
-      allowNull: false,
-      defaultValue: "user",
-    },
-    passwordHash: {
-      type: DataTypes.CHAR(60),
-      allowNull: false,
-    },
-    twoFactorEnabled: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false,
-    },
-    twoFactorSecret: {
-      type: DataTypes.CHAR(32),
-      allowNull: true,
-    },
-    emailVerified: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false,
-    },
-    emailVerifiedAt: {
-      type: DataTypes.DATE,
-      allowNull: true,
-    },
-  },
-  {
-    tableName: "users",
-    paranoid: true,
-    indexes: [{ unique: true, fields: ["email"] }, { fields: ["role"] }],
-  }
+	"User",
+	{
+		id: {
+			type: DataTypes.BIGINT.UNSIGNED,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		email: {
+			type: DataTypes.STRING(190),
+			allowNull: false,
+			validate: { isEmail: true },
+		},
+		role: {
+			type: DataTypes.ENUM("user", "admin"),
+			allowNull: false,
+			defaultValue: "user",
+		},
+		passwordHash: {
+			type: DataTypes.CHAR(60),
+			allowNull: false,
+		},
+		twoFactorEnabled: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: false,
+		},
+		twoFactorSecret: {
+			type: DataTypes.CHAR(20),
+			allowNull: true,
+		},
+		emailVerified: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: false,
+		},
+		emailVerifiedAt: {
+			type: DataTypes.DATE,
+			allowNull: true,
+		},
+	},
+	{
+		tableName: "users",
+		paranoid: true,
+		indexes: [{ unique: true, fields: ["email"] }, { fields: ["role"] }],
+	}
 );
