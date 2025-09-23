8ce244c6b09a729049007e89e3267eecf8e5c84a
Carlos Moya <mrcamoga@gmail.com>
2025-09-23 12:04:40 +0200

    Added CIF to models

--- src/models/company.js
+++ src/models/company.js
@@ -23,6 +23,30 @@ const Company = sequelize.define('Company', {
 		type: DataTypes.STRING(40),
 		allowNull: false
 	},
+	CIF: {
+		type: DataTypes.CHAR(9),
+		allowNull: false,
+		unique: true,
+		validate: {
+			validCIF(cif) {
+				if(!cif || typeof cif != "string" || cif.length != 9) return false;
+				if(!/^[A-HJNP-SU-W]\d{7}[0-9A-J]$/.test(cif)) return false;
+				if((cif.substring(1,3) === "00" || cif[0].match(/[NP-SW]/)) && !cif[8].match(/[A-J]/)) return false;
+				if(cif[0].match(/[ABEH]/) && !cif[8].match(/[0-9]/)) return false;
+
+				let control = cif[8];
+				if(control.match(/[A-J]/)) control = (cif[8].charCodeAt(0)-54)%10;
+				const sumEven = (+cif[2]) + (+cif[4]) + (+cif[6]);
+				let sumOdd = 0;
+				for(let i = 1; i <= 7; i+=2) {
+					const prod = 2*(+cif[i]);
+					sumOdd += prod < 10 ? prod:prod-9;
+				}
+				const res = (10-(sumEven + sumOdd)%10)%10;
+				return res == control;
+			}
+		}
+	},
 	subscription_active: {
 		type: DataTypes.BOOLEAN,
 		allowNull: false,
